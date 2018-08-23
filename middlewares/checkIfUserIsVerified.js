14d852f728eefb0819e609fd8dff2b65a77dc2a8
Onengiye Richard <klevamane@gmail.com>
2018-08-23 12:27:20 +0100

    bug(application): fix issue with loading the app - fix issue with loading the application
    [Fixes #159403248]

--- middlewares/checkIfUserIsVerified.js
+++ middlewares/checkIfUserIsVerified.js
@@ -1,15 +1,19 @@
 import { User } from '../models';
 
 exports.checkIfUserIsVerified = async (req, res, next) => {
-  const { email } = req.body.user;
-  const usertoBeVerified = await User.find({ where: { email } });
-  if (usertoBeVerified && usertoBeVerified.isverified === false) {
-    return res.status(400).json({
-      success: false,
-      errors: {
-        body: ['You account has not been activated'],
-      }
-    });
+  try {
+    const { email } = req.body.user;
+    const usertoBeVerified = await User.find({ where: { email } });
+    if (usertoBeVerified && usertoBeVerified.isverified === false) {
+      return res.status(400).json({
+        success: false,
+        errors: {
+          body: ['You account has not been activated'],
+        }
+      });
+    }
+    next();
+  } catch (err) {
+    next(err);
   }
-  next();
 };
